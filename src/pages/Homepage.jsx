3e77dd65708c5fc5be6996facf430c272fabfde4
Silvia <silvia8gasparini@gmail.com>
2025-07-10 11:05:21 +0200

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,38 +1,19 @@
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
 import { Container } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import { fetchColorByMicroSeasonId } from "../redux/selectedColorSlice";
-import { fetchBooksByMicroSeasonId } from "../redux/booksSlice";
-
 import CustomNavbar from "../components/CustomNavbar";
 import CustomFooter from "../components/CustomFooter";
 import Welcome from "../components/Welcome";
 import ColorSection from "../components/ColorSection";
 import BookSection from "../components/BookSection";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Homepage() {
-  const dispatch = useDispatch();
-
-  const microSeasonId = useSelector(
-    (state) => state.currentMicroSeason.microSeason?.id
-  );
-
-  useEffect(() => {
-    if (microSeasonId) {
-      dispatch(fetchColorByMicroSeasonId(microSeasonId));
-      dispatch(fetchBooksByMicroSeasonId(microSeasonId));
-    }
-  }, [dispatch, microSeasonId]);
-
   return (
     <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
       <CustomNavbar />
       <Container fluid className="flex-grow-1 px-4">
         <Welcome />
         <ColorSection />
-        {microSeasonId && <BookSection microSeasonId={microSeasonId} />}
+        <BookSection />
       </Container>
       <CustomFooter />
     </div>
